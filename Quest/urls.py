4a31e33b3700604c3723d1e7c11b69b1294166ae
M254-bto <ngechamike26@gmail.com>
2025-07-31 23:32:27 +0300

    Swagger

--- Quest/urls.py
+++ Quest/urls.py
@@ -16,8 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+from django.conf.urls.static import static
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AaideCare API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],  # Ensures Swagger is accessible
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('attendance.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
